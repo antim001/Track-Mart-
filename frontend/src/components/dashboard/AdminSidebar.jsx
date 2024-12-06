058485cb65f565e6d2bf0906f60f7e7f3e1b6270
Antim <antimsarker0@gmail.com>
2024-12-06 17:45:31 +0600

    dashboard overview

--- frontend/src/components/dashboard/AdminSidebar.jsx
+++ frontend/src/components/dashboard/AdminSidebar.jsx
@@ -4,31 +4,33 @@ import {FaTachometerAlt,FaUsers,FaBuilding} from 'react-icons/fa'
 const AdminSidebar=()=> {
   return (
     <div className='bg-gray-800 text-white h-screen fixed left-0 top-0 bottom-0 space-y-2 w-64'>
-   <div className='bg-teal-900 h-12 flex items-center justify-center'>
+   <div className='bg-teal-600 h-12 flex items-center justify-center'>
    <h3 className='text-2xl text-center font-pacific'>Track Mart</h3>
    </div>
    <div>
-   <NavLink to='/admin-dashboard'>
+   <NavLink to='/admin-dashboard'
+   className={({isActive})=>`${isActive?"bg-teal-500":""} flex items-center space-x-4  py-2.5 px-4 rounded`}
+   >
    <FaTachometerAlt/>
    <span>Dashboard</span>
-   </NavLink>
-   <NavLink to='/admin-dashboard'>
+   </NavLink >
+   <NavLink to='/admin-dashboard' className='flex items-center space-x-4  py-2.5 px-4 rounded'>
    <FaUsers/>
    <span>Employess</span>
    </NavLink>
-   <NavLink to='/admin-dashboard'>
+   <NavLink to='/admin-dashboard'className='flex items-center space-x-4  py-2.5 px-4 rounded'>
    <FaBuilding/>
    <span>Department</span>
    </NavLink>
-   <NavLink to='/admin-dashboard'>
+   <NavLink to='/admin-dashboard' className='flex items-center space-x-4  py-2.5 px-4 rounded'>
    <FaTachometerAlt/>
    <span>Leave</span>
    </NavLink>
-   <NavLink to='/admin-dashboard'>
+   <NavLink to='/admin-dashboard'className='flex items-center space-x-4  py-2.5 px-4 rounded'>
    <FaTachometerAlt/>
    <span>Salary</span>
    </NavLink>
-   <NavLink to='/admin-dashboard'>
+   <NavLink to='/admin-dashboard'className='flex items-center space-x-4  py-2.5 px-4 rounded'>
    <FaTachometerAlt/>
    <span>Settings</span>
    </NavLink>
--- frontend/src/pages/AdminDashboard.jsx
+++ frontend/src/pages/AdminDashboard.jsx
@@ -1,13 +1,21 @@
 import React from 'react'
 import {useAuth} from '../context/authContext'
 import AdminSidebar from '../components/dashboard/AdminSidebar.jsx'
+import Navbar from '../components/Navbar.jsx'
+import AdminSummary from '../components/AdminSummary.jsx'
 function AdminDashboard() {
   const {user,loading}=useAuth();
 
   
   
   return (
+    <div className='flex'>
     <AdminSidebar/>
+    <div className='flex-1 ml-64 bg-gray-100 h-screen'>
+    <Navbar/>
+    <AdminSummary/>
+    </div>
+    </div>
   )
 }
 
